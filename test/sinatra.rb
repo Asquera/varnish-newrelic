896ef165b7daf6567f8ccb3b1f1d225ad91e6f55
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2012-07-27 12:11:11 +0200

    sinatra based web service that outputs the X-Request-Start header

---
+++ test/sinatra.rb
@@ -0,0 +1,11 @@
+require 'sinatra'
+
+get '/' do
+  if request["X-Request-Start"]
+    request_start = request["X-Request-Start"]
+    "X-Request-Start: #{request_start}"
+  else
+    "No X-Request-Start found"
+  end
+end
+
